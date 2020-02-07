40897ea9418392d4ebb213e0fbe6fe8a8c54ad98
thines <skwebbies@hotmail.co.uk>
2020-02-07 23:15:02 +0100

    adds main admin page

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+
+<head>
+	<script src="/dmxAppConnect/dmxAppConnect.js"></script>
+	<base href="/admin/">
+	<meta charset="UTF-8">
+	<title>Untitled Document</title>
+	<script src="/js/jquery-3.4.1.slim.min.js"></script>
+	<link rel="stylesheet" href="/fontawesome4/css/font-awesome.min.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="/bootstrap/4/css/bootstrap.min.css" />
+	<script src="/dmxAppConnect/dmxBootstrap4Navigation/dmxBootstrap4Navigation.js" defer=""></script>
+</head>
+
+<body is="dmx-app" id="index" style="background: #00274f;">
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">VSFX Admin Panel</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse1" aria-controls="collapse1" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div id="collapse1" class="collapse navbar-collapse">
+				<div class="navbar-nav">
+					<a class="nav-item nav-link" href="#">Exchange Rates</a>
+					<a class="nav-item nav-link" href="#">Currencies</a>
+					<a class="nav-item nav-link" href="#">Agents</a>
+					<a class="nav-item nav-link" href="#">Users</a>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<script src="/bootstrap/4/js/popper.min.js"></script>
+	<script src="/bootstrap/4/js/bootstrap.min.js"></script>
+</body>
+
+</html>
