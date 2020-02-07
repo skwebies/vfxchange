d4768d370a091925ccffdede385da2c525cba4fd
thines <skwebbies@hotmail.co.uk>
2020-02-07 23:16:05 +0100

    adds bootstrap, app connect and database connection

--- dmxConnect/modules/Connections/db.php
+++ dmxConnect/modules/Connections/db.php
@@ -9,16 +9,7 @@ $exports = <<<'JSON'
     "databaseType": "MySQL",
     "connectionString": "mysql:host=db;sslverify=false;port=3306;dbname=vfxchange;user=db_user;password=VD8L9Wwm;charset=utf8",
     "limit": 1000,
-    "debug": false,
-    "meta": {
-      "allTables": [
-        "cars",
-        "countries",
-        "images",
-        "users"
-      ],
-      "allViews": []
-    }
+    "debug": false
   }
 }
 JSON;
